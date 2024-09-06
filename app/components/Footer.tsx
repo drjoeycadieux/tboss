8caf9798b7cb4b5c7d343bac2bea0e8c55f3e894
Joey Cadieux <instructor.joeycadieux@gmail.com>
2024-09-06 18:26:28 +0000

    inm.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,43 @@
 import styles from "../home.module.css";
 
-export default function Footer() {
+const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <p>2024 - &copy; Copyright Tboss Company. </p>
-    </div>
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <div className={styles.footerColumn}>
+          <h3>Inside Company</h3>
+          <ul>
+            <li>
+              <a href="#">Careers</a>
+            </li>
+            <li>
+              <a href="#">Security</a>
+            </li>
+            <li>
+              <a href="#">Top Technologies</a>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.footerColumn}>
+          <h3>Link</h3>
+          <ul>
+            <li>
+              <a href="#">Help</a>
+            </li>
+            <li>
+              <a href="#">Support</a>
+            </li>
+            <li>
+              <a href="#">Other</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.footerBottom}>
+        <p>© 2024 Tboss Software Foundations. All rights reserved.</p>
+      </div>
+    </footer>
   );
-}
+};
+
+export default Footer;
